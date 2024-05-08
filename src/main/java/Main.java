c99814931813216cac0091f6298b17a1a4fbd928
lukaszruman <161606263+lukaszruman@users.noreply.github.com>
2024-05-08 13:29:56 +0000

    cx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
+      s.addStudent(new Student("Krzysztolf", 20));
       s.addStudent(new Student("Janusz", 40));
 
       var students = s.getStudents();
